e025494e0d714388dcc9cb688bf26774883634a5
Jason Huang <wowjason@gmail.com>
2023-05-03 22:15:22 +0000

    staged and commit first time

--- src/App.jsx
+++ src/App.jsx
@@ -24,12 +24,12 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
+  name: "Jason Huang",
+  title: "Data Reporting and Analytics Consultant",
+  email: "jason.huang@example.com",
   gitHub: "microsoft",
   instagram: "microsoft",
-  linkedIn: "satyanadella",
+  linkedIn: "KPQM",
   medium: "",
   twitter: "microsoft",
   youTube: "Code",
